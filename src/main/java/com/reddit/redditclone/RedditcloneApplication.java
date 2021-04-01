cca159e09407f808227e16a7af8bd3e1d7e8d1cf
satyaroshan <satyaroshan2@gmail.com>
2021-04-01 09:45:07 +0530

    Adding domain to project

--- src/main/java/com/reddit/redditclone/RedditcloneApplication.java
+++ src/main/java/com/reddit/redditclone/RedditcloneApplication.java
@@ -1,14 +1,50 @@
 package com.reddit.redditclone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@EnableConfigurationProperties(UserdefinedProperties.class)
 public class RedditcloneApplication {
 
+	@Autowired 
+	private UserdefinedProperties user;
+	
+	private static final Logger log=LoggerFactory.getLogger(RedditcloneApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RedditcloneApplication.class, args);
 		System.out.println("Welcome to SpringBoot Project!");
 	}
+	
+	@Bean
+	@Profile("dev")
+	CommandLineRunner runner()
+	{
+		return args->{
+			System.out.println(user.getWelMssg());
+		};
+	}
+	
+	
+	@Bean
+	CommandLineRunner runner1()
+	{
+		return args->{
+			log.debug("Debug");
+			log.info("Info");
+			log.error("error");
+			log.warn("Warning");
+		};
+	}
+	
+	
 
 }
---
+++ src/main/java/com/reddit/redditclone/domain/Vote.java
@@ -0,0 +1,21 @@
+package com.reddit.redditclone.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+	
+	@Id
+	@GeneratedValue
+	private Long Id;
+	private int vote;
+	
+
+}
